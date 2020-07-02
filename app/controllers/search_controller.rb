6e9a318f06a2db862cff82d084d3302866fe7f6b
Taylor Keller <takeller17@gmail.com>
2020-07-02 10:47:07 -0600

    Finish Refactor

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,7 @@
+class SearchController < ApplicationController
+
+  def index
+    search_results = SearchResults.new
+    @members = search_results.members(params[:state])
+  end
+end
---
+++ spec/features/user_can_search_by_state_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+feature "user can search for house members" do
+
+  scenario "user submits valid state name" do
+    # As a user
+   # When I visit "/"
+   visit '/'
+
+   select "Colorado", from: :state
+   # And I select "Colorado" from the dropdown
+   click_on "Locate Members of the House"
+   # And I click on "Locate Members from the House"
+   expect(current_path).to eq(search_path)
+   # Then my path should be "/search" with "state=CO" in the parameters
+   expect(page).to have_content("7 Results")
+   # And I should see a message "7 Results"
+   expect(page).to have_css(".member", count: 7)
+   # And I should see a list of 7 the members of the house for Colorado
+
+   within(first(".member")) do
+     expect(page).to have_css(".name")
+     expect(page).to have_css(".role")
+     expect(page).to have_css(".party")
+     expect(page).to have_css(".district")
+   end
+   # And they should be ordered by seniority from most to least
+   # And I should see a name, role, party, and district for each member
+  end
+end
